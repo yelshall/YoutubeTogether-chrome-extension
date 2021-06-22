dec136eabe3367ce6456c278b05d8c3fec9e3e32
yelshall <youssufshall@gmail.com>
2021-06-22 23:08:17 +0400

    Added basic UI functionality

--- Pages/scripts/watch.js
+++ Pages/scripts/watch.js
@@ -1,11 +1,43 @@
+//Global Variables
 const chatcontainer = document.getElementById('chat');
+const inputChat = document.getElementById('submit-button');
+const copyBtn = document.getElementById('copy-btn');
+const linkBox = document.getElementById('share-url');
 
+//Append Message to chat container
 var addMessage = function(name, type, message) {
     if (type == "createjoinleave") {
         let chatMessage = '<p id="message"><i><b>' + name + '<b> ' + message + '</i></p>';
         chatcontainer.innerHTML += chatMessage;
+    } else if (type == "message") {
+        let chatMessage = '<p><b>' + name + '</b>: ' + message + '</p>';
+        chatcontainer.innerHTML += chatMessage;
     }
+
+    chatcontainer.scrollTop = chatcontainer.scrollHeight;
 };
 
-addMessage("HIIIII", "createjoinleave", "Hello");
-addMessage("Omar", "createjoinleave", "Hello");
+//Set URL in linkBox
+var changeLink = function(urlLink) {
+    linkBox.value = urlLink;
+};
+
+//Add button functionality for messaging
+inputChat.addEventListener('click', (event) => {
+    event.preventDefault();
+    let message = document.getElementById('message-input');
+
+    if (message.value != "") {
+        addMessage("placeHolderName", "message", message.value);
+        message.value = ""
+    }
+});
+
+//Add button functionality for copying the URL
+copyBtn.addEventListener('click', (event) => {
+    linkBox.select();
+    document.execCommand('copy');
+});
+
+//Placeholder code
+changeLink("PlaceHolder URL");
