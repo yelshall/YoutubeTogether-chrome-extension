3e7d1ef40ad0bb79f9d92955f47d488864a31d52
yelshall <youssufshall@gmail.com>
2021-06-24 06:20:37 +0400

    Added basic messaging functionality over server

--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,13 @@
-var startScript = function() {
-    const ytVideo = document.querySelector("video");
+const ytVideo = document.querySelector("video");
 
-    ytVideo.addEventListener('play', () => {
-        chrome.runtime.sendMessage({ type: "play", data: { timeStamp: ytVideo.currentTime }, id: -1 });
-    });
+ytVideo.addEventListener('play', () => {
+    chrome.runtime.sendMessage({ type: "play", data: { timeStamp: ytVideo.currentTime }, id: -1 });
+});
 
-    ytVideo.addEventListener('pause', () => {
-        chrome.runtime.sendMessage({ type: "pause", data: { timeStamp: ytVideo.currentTime }, id: -1 });
-    });
+ytVideo.addEventListener('pause', () => {
+    chrome.runtime.sendMessage({ type: "pause", data: { timeStamp: ytVideo.currentTime }, id: -1 });
+});
 
-    ytVideo.addEventListener('timeupdate', () => {
-        chrome.runtime.sendMessage({ type: "durationChange", data: { timeStamp: ytVideo.currentTime }, id: -1 });
-    });
-};
-
-startScript();
+ytVideo.addEventListener('timeupdate', () => {
+    chrome.runtime.sendMessage({ type: "durationChange", data: { timeStamp: ytVideo.currentTime }, id: -1 });
+});
